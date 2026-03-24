6e45740ced24e45562c1a094cba3aac98ee463e4
alexandrius <alexander.pa@exodus.io>
2026-03-24 11:36:32 +0400

    feat: benchmarks

--- benchmark/results.js
+++ benchmark/results.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line no-unused-vars
 const results = {
-  Shakl: 0.029_811_424_241_520_11,
-  Emotion: 0.037_209_380_871_224_86,
-  'Styled Components': 0.046_611_316_695_394_33,
+  Shakl: 0.012900377880803662,
+  Emotion: 0.01546536950109481,
+  'Styled Components': 0.021032191611905397,
 }
